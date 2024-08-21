fc44584f585f2e84be83533f95e0f22a60b6335a
june lee <melodysdreamj@gmail.com>
2024-08-21 21:15:05 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'uihub/uihub_netflix_home/main.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: NewView(),
+    ),
+  ));
+}
